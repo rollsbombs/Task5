bfa0344d88b91da54c68464d841dbb15937c7deb
Eleonora Kuranova <aflyalf@gmail.com>
2025-08-29 13:18:35 +0300

    доделала задание2

--- src/Task5/Task5_1.java
+++ src/Task5/Task5_1.java
@@ -11,7 +11,7 @@ public class Task5_1 {
         try (Scanner scanner = new Scanner(System.in)) {
             SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
             try {
-                System.out.println("Введите дату в формате дд.мм.гггг");
+                System.out.println("Введите дату в формате дд.мм.гггг:");
                 String dateStr1 = scanner.nextLine();
                 Date date1 = sdf.parse(dateStr1);
                 GregorianCalendar calendar = new GregorianCalendar();
@@ -20,8 +20,8 @@ public class Task5_1 {
                 Date date45 = calendar.getTime();
                 System.out.println("Дата после увеличения на 45 дней: " + sdf.format(date45));
                 calendar.setTime(date1);
-                calendar.set(GregorianCalendar.DAY_OF_MONTH, 1);
                 calendar.set(GregorianCalendar.MONTH, GregorianCalendar.JANUARY);
+                calendar.set(GregorianCalendar.DAY_OF_MONTH, 1);
                 Date begindate = calendar.getTime();
                 System.out.println("Дата после сдвига на начало года: " + sdf.format(begindate));
                 calendar.setTime(date1);
@@ -35,7 +35,7 @@ public class Task5_1 {
                 }
                 Date date10 = calendar.getTime();
                 System.out.println("Дата после увеличения на 10 рабочих дней: " + sdf.format(date10));
-                System.out.println("Введите вторую дату в формате дд.мм.гггг");
+                System.out.println("Введите вторую дату в формате дд.мм.гггг:");
                 String dateStr2 = scanner.nextLine();
                 Date date2 = sdf.parse(dateStr2);
                 calendar.setTime(date1);
@@ -47,7 +47,7 @@ public class Task5_1 {
                     }
                     calendar.add(GregorianCalendar.DAY_OF_MONTH, 1);
                 }
-                System.out.println("Количество рабочих дней между введёнными датами: " + sdf.format(workDays));
+                System.out.println("Количество рабочих дней между введёнными датами: " + workDays);
             } catch (ParseException e) {
                 System.out.println("Ошибка: Неверный формат даты. Используйте формат 'дд.мм.гггг'");
             }
